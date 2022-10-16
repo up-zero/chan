092043e681cd308a095b0aadf02958e746af426e
GetcharZp <2272795814@qq.com>
2022-10-16 21:36:30 +0800

    feat 负载均衡

--- define/template.go
+++ define/template.go
@@ -10,8 +10,9 @@ type Server struct {
 	SslCert    string `json:"ssl_cert"`    // ssl 证书
 	SslKey     string `json:"ssl_key"`     // ssl key
 	Location   []struct {
-		Path  string   `json:"path"`  // 父级路由地址
-		Index []string `json:"index"` // 首页包含的地址
-		Root  string   `json:"root"`  // 静态资源文件地址
+		Path      string   `json:"path"`       // 父级路由地址
+		Index     []string `json:"index"`      // 首页包含的地址
+		Root      string   `json:"root"`       // 静态资源文件地址
+		ProxyPass string   `json:"proxy_pass"` // 反向代理地址，与静态代理互斥，同时配置时只有一种生效
 	} `json:"location"` // 请求地址
 }
